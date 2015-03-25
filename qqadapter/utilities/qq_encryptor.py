8f1a780a77625e523bb237951898748c90b99378
changyuf <advcorydon@126.com>
2015-03-25 09:08:12 +0800

    add encryptor on windows

--- qqadapter/utilities/qq_encryptor.py
+++ qqadapter/utilities/qq_encryptor.py
@@ -1,6 +1,12 @@
 __author__ = 'changyuf'
 
-import spidermonkey
+import platform
+
+if platform.system() == "Windows":
+    import win32com.client
+else:
+    import spidermonkey
+
 from qqadapter.bean.qquser import QQAccount
 
 
@@ -12,14 +18,32 @@ class QQEncryptor:
 
     @staticmethod
     def encrypt2(qq_account, verify_code):
+        if platform.system() == "Windows":
+            return QQEncryptor.__encrypt_windows(qq_account, verify_code)
+        else:
+            return QQEncryptor.__encrypt_linux(qq_account, verify_code)
+
+    @staticmethod
+    def __encrypt_linux(qq_account, verify_code):
         rt = spidermonkey.Runtime()
         cx = rt.new_context()
         fs = open(QQEncryptor.mq_comm_js, "r")
         func = cx.execute(fs.read())
         return func(qq_account.password, qq_account.user_name, verify_code)
 
+    @staticmethod
+    def __encrypt_windows(qq_account, verify_code):
+        js = win32com.client.Dispatch('MSScriptControl.ScriptControl')
+        #js = win32com.client.Dispatch('MSScriptControl.ScriptControl')
+        js.Language = 'JavaScript'
+        js.AllowUI  = False
+        fs = open("qq_comm.js", "r")
+        js.AddCode(fs.read())
+        return js.Run("getPassword", qq_account.password, qq_account.user_name, verify_code)
+
 
 if __name__ == '__main__':
     account = QQAccount("3047296752", "123456789")
+
 
     print QQEncryptor.encrypt2(account, "UFDS")
